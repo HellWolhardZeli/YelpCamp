42879ac0311d1b7944adc3228cc80dc1762cd991
HellWolhardZeli <aditya11maharana@gmail.com>
2020-04-22 18:55:39 +0530

    Major changes

--- app.js
+++ app.js
@@ -1,62 +1,72 @@
-var express = require ('express');
-var app = express ();
-var parser = require ('body-parser');
-var mongoose = require ('mongoose');
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  LocalStrategy = require("passport-local"),
+  flash = require("connect-flash"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  session = require("express-session"),
+  seedDB = require("./seeds"),
+  methodOverride = require("method-override");
+// configure dotenv
+require("dotenv").load();
 
-mongoose.connect ('mongodb://localhost/YelpCamp', {useNewUrlParser: true});
+//requiring routes
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
-app.use (parser.urlencoded ({extended: true}));
+// assign mongoose promise library and connect to database
+mongoose.Promise = global.Promise;
 
-var campGroundsSchema = new mongoose.Schema ({
-  name: String,
-  image: String,
-  description: String,
+const databaseUri = process.env.MONGODB_URI || "mongodb://localhost/yelp_camp";
+
+mongoose
+  .connect(databaseUri, { useMongoClient: true })
+  .then(() => console.log(`Database connected`))
+  .catch((err) => console.log(`Database connection error: ${err.message}`));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(cookieParser("secret"));
+//require moment
+app.locals.moment = require("moment");
+// seedDB(); //seed the database
+
+// PASSPORT CONFIGURATION
+app.use(
+  require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
-var campGrounds = mongoose.model ('campGrounds', campGroundsSchema);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.get ('/', function (req, res) {
-  res.render ('home.ejs');
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log("The YelpCamp Server Has Started!");
 });
-
-app.get ('/campGrounds', function (req, res) {
-  campGrounds.find ({}, function (err, campGrounds) {
-    if (err) {
-      console.log (err);
-    } else {
-      res.render ('index.ejs', {campGrounds: campGrounds});
-    }
-  });
-});
-
-app.get ('/campGrounds/new', function (req, res) {
-  res.render ('new.ejs');
-});
-
-app.get ('/campGrounds/:id', function (req, res) {
-  campGrounds.findById (req.params.id, function (err, foundCampGround) {
-    if (err) {
-      console.log (err);
-    } else {
-      res.render ('show.ejs', {campGrounds: foundCampGround});
-    }
-  });
-});
-
-app.post ('/campGrounds', function (req, res) {
-  var name = req.body.name;
-  var image = req.body.image;
-  var newCampGrounds = {name: name, image: image};
-  campGrounds.create (newCampGrounds, function (err, campGrounds) {
-    if (err) {
-      console.log (err);
-    } else {
-      res.redirect ('/campGrounds');
-    }
-  });
-});
-
-// Starts Server at port 3000
-app.listen (3000), function () {
-  console.log ('Server has started');
-};
+// app.listen(3000);
